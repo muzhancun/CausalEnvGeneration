eead1352957ea1c1fe03826850b2b27dd6f1e889
caishaofei-mus1 <1744260356@qq.com>
2024-11-11 20:01:16 +0800

    update callbacks for minecraft simulator

--- minestudio/simulator/minerl/entry.py
+++ minestudio/simulator/minerl/entry.py
@@ -1,16 +1,17 @@
 '''
 Date: 2024-11-11 05:20:17
-LastEditors: caishaofei caishaofei@stu.pku.edu.cn
-LastEditTime: 2024-11-11 07:55:26
-FilePath: /MineStudio/minestudio/simulator/minerl/entry.py
+LastEditors: caishaofei-mus1 1744260356@qq.com
+LastEditTime: 2024-11-11 20:00:38
+FilePath: /MineStudio/scratch/caishaofei/workspace/MineStudio/minestudio/simulator/minerl/entry.py
 '''
 
 import os
 import cv2
 import argparse
+import numpy as np
 import gymnasium
 from gymnasium import spaces
-import numpy as np
+from copy import deepcopy
 from typing import Dict, List, Tuple, Union, Sequence, Mapping, Any, Optional, Literal
 
 from minestudio.utils.vpt_lib.actions import ActionTransformer
@@ -18,6 +19,16 @@ from minestudio.utils.vpt_lib.action_mapping import CameraHierarchicalMapping
 from minestudio.simulator.minerl.utils.inventory import map_slot_number_to_cmd_slot
 from minestudio.simulator.minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
 from minestudio.simulator.minerl.callbacks import MinecraftCallback
+
+ACTION_TRANSFORMER_KWARGS = dict(
+    camera_binsize=2,
+    camera_maxval=10,
+    camera_mu=10,
+    camera_quantization_scheme="mu_law",
+)
+
+action_mapper = CameraHierarchicalMapping(n_camera_bins=11)
+action_transformer = ActionTransformer(**ACTION_TRANSFORMER_KWARGS)
 
 if not os.path.exists(os.path.join(os.path.dirname(__file__), "MCP-Reborn")):
     print("Detecting missing MCP-Reborn, downloading...")
@@ -30,7 +41,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), "MCP-Reborn")):
 class MinecraftSim(gymnasium.Env):
     
     def __init__(
-        self, 
+        self,  
         obs_size: Tuple[int, int] = (224, 224),         # the resolution of the observation (cv2 resize)
         action_type: Literal['env', 'agent'] = 'agent', # the style of the action space
         render_size: Tuple[int, int] = (640, 360),      # the original resolution of the game is 640x360
@@ -59,10 +70,14 @@ class MinecraftSim(gymnasium.Env):
             inventory = inventory,
             preferred_spawn_biome = preferred_spawn_biome, 
         ).make()
+        self.already_reset = False
     
     def step(self, action: Dict[str, Any]) -> Tuple[np.ndarray, float, bool, bool, Dict[str, Any]]:
         for callback in self.callbacks:
             action = callback.before_step(self, action)
+        if self.action_type == 'agent':
+            action = action_mapper.to_factored(action)
+            action = action_transformer.policy2env(action)
         obs, reward, done, info = self.env.step(action.copy()) 
         terminated, truncated = done, done
         obs, info = self._wrap_obs_info(obs, info)
@@ -77,6 +92,7 @@ class MinecraftSim(gymnasium.Env):
             reset_flag = callback.before_reset(self, reset_flag)
         if reset_flag: # hard reset
            self.env.reset()
+           self.already_reset = True
         for _ in range(self.num_empty_frames): # skip the frames to avoid the initial black screen
             action = self.env.action_space.no_op()
             obs, reward, done, info = self.env.step(action)
@@ -95,8 +111,8 @@ class MinecraftSim(gymnasium.Env):
     def noop_action(self) -> Dict[str, Any]:
         if self.action_type == 'agent':
             return {
-                "buttons": [0],
-                "camera": [60],
+                "buttons": np.array([0]),
+                "camera": np.array([60]),
             }
         else:
             return self.env.action_space.no_op()
@@ -124,25 +140,84 @@ class MinecraftSim(gymnasium.Env):
                 "camera":  gymnasium.spaces.MultiDiscrete([121]), 
             })
         elif self.action_type == 'env':
-            # using factored action space
-            ...
+            return gymnasium.spaces.Dict({
+                'attack': gymnasium.spaces.Discrete(2),
+                'back': gymnasium.spaces.Discrete(2),
+                'forward': gymnasium.spaces.Discrete(2),
+                'jump': gymnasium.spaces.Discrete(2),
+                'left': gymnasium.spaces.Discrete(2),
+                'right': gymnasium.spaces.Discrete(2),
+                'sneak': gymnasium.spaces.Discrete(2),
+                'sprint': gymnasium.spaces.Discrete(2),
+                'use': gymnasium.spaces.Discrete(2),
+                'hotbar.1': gymnasium.spaces.Discrete(2),
+                'hotbar.2': gymnasium.spaces.Discrete(2),
+                'hotbar.3': gymnasium.spaces.Discrete(2),
+                'hotbar.4': gymnasium.spaces.Discrete(2),
+                'hotbar.5': gymnasium.spaces.Discrete(2),
+                'hotbar.6': gymnasium.spaces.Discrete(2),
+                'hotbar.7': gymnasium.spaces.Discrete(2),
+                'hotbar.8': gymnasium.spaces.Discrete(2),
+                'hotbar.9': gymnasium.spaces.Discrete(2),
+                'inventory': gymnasium.spaces.Discrete(2),
+                'camera': gymnasium.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),
+            })
         else:
             raise ValueError(f"Unknown action type: {self.action_type}")
     
     @property
     def observation_space(self) -> spaces.Dict:
-        width, height = self.obs_size
+        height, width = self.obs_size
         return gymnasium.spaces.Dict({
-            "image": gymnasium.spaces.Box(low=0, high=255, shape=(width, height, 3), dtype=np.uint8)
+            "image": gymnasium.spaces.Box(low=0, high=255, shape=(height, width, 3), dtype=np.uint8)
         })
 
 if __name__ == '__main__':
-    from minestudio.simulator.minerl.callbacks import SpeedTestCallback
+    from minestudio.simulator.minerl.callbacks import (
+        SpeedTestCallback, 
+        RecordCallback, 
+        SummonMobsCallback, 
+        MaskActionsCallback, 
+        RewardsCallback, 
+        CommandsCallback, 
+        TaskCallback,
+        FastResetCallback
+    )
     sim = MinecraftSim(
-        callbacks=[SpeedTestCallback(50)]
+        action_type="env",
+        callbacks=[
+            SpeedTestCallback(50), 
+            SummonMobsCallback([{'name': 'cow', 'number': 10, 'range_x': [-5, 5], 'range_z': [-5, 5]}]),
+            MaskActionsCallback(inventory=0, camera=np.array([0., 0.])), 
+            RecordCallback(record_path="/nfs-shared-2/shaofei/output", fps=30),
+            RewardsCallback([{
+                'event': 'kill_entity', 
+                'objects': ['cow', 'sheep'], 
+                'reward': 1.0, 
+                'identity': 'kill sheep or cow', 
+                'max_reward_times': 5, 
+            }]),
+            CommandsCallback(commands=[
+                '/give @p minecraft:iron_sword 1',
+                '/give @p minecraft:diamond 64',
+            ]), 
+            FastResetCallback(
+                biomes=['mountains'],
+                random_tp_range=1000,
+            ), 
+            TaskCallback([
+                {'name': 'chop', 'text': 'mine the oak logs'}, 
+                {'name': 'diamond', 'text': 'mine the diamond ore'},
+            ])
+        ]
     )
     obs, info = sim.reset()
-    action = sim.noop_action()
-    for i in range(200):
+    action_space = sim.action_space
+    print(action_space)
+    for i in range(300):
+        action = action_space.sample()
+        # action = sim.noop_action()
+        if (i+1) % 150 == 0:
+            sim.reset()
         obs, reward, terminated, truncated, info = sim.step(action)
     sim.close()
